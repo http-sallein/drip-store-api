31ea6cd8e8693db1721ab6deb8f6f8fcc6a79f99
unknown <MATHEUS0376@digitalcollegesul.local>
2024-08-14 17:51:46 -0300

    codando

---
+++ src/routes/userRoute.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/userController';
+
+
+const router = express.Router();
+
+// Rota para obter todos os usuários
+router.get('/users', getAllUsers);
+
+// Rota para obter um usuário por ID
+router.get('/users/:id', getUserById);
+
+// Rota para criar um novo usuário
+router.post('/users', createUser);
+
+// Rota para atualizar um usuário por ID
+router.put('/users/:id', updateUser);
+
+// Rota para deletar um usuário por ID
+router.delete('/users/:id', deleteUser);
+
+export default router;
